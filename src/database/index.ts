1ae78f9e1e4cdda44fea06889b1c3721ecefd78a
SnowFox <kwebdev225@gmail.com>
2021-01-22 14:14:57 +0800

    no message

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,24 +1,11 @@
 const { MONGO_HOST, MONGO_PORT, MONGO_DATABASE } = process.env;
 
-export const db =
-  MONGO_HOST === 'localhost'
-    ? {
-        url: `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}`,
-        options: {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-          useFindAndModify: false,
-          useCreateIndex: true,
-        },
-      }
-    : {
-        url: `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}`,
-        options: {
-          user: 'dankhcqz',
-          pass: 'night225@@%',
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-          useFindAndModify: false,
-          useCreateIndex: true,
-        },
-      };
+export const db = {
+  url: `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}`,
+  options: {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  },
+};
