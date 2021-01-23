61d0fa67a4839c5df260b331de95786696c5e10b
SnowFox <kwebdev225@gmail.com>
2021-01-23 10:41:52 +0800

    Add mongo auth

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,8 +1,11 @@
-const { MONGO_HOST, MONGO_PORT, MONGO_DATABASE } = process.env;
+const { MONGO_HOST, MONGO_PORT, MONGO_DATABASE, MONGO_USER, MONGO_PWD } = process.env;
 
 export const db = {
   url: `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}`,
   options: {
+    authSource: 'admin',
+    user: MONGO_USER,
+    pass: MONGO_PWD,
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
